d2f227d366c9b99b4e0b646c35cd389905affb80
armurray <>
2021-02-04 13:24:49 -0500

    Leaf map

---
+++ Leaf_Map/Leaf_Map.R
@@ -0,0 +1,55 @@
+library(googlesheets4)
+library(tidyverse)
+library(sf)
+library(here)
+library(leaflet)
+library(leaflet.extras)
+library(htmltools)
+library(htmlwidgets)
+
+# Set Mapbox token
+Sys.setenv('MAPBOX_TOKEN' = 'pk.eyJ1IjoiYXJtdXJyYXkxODkiLCJhIjoiY2trcjFhNG4yMG1sdjJ3bnhsY3p0dnlpaiJ9.1z2hrGSZT0vA9P4gXAcezg')
+
+# Read in Data from google sheets
+
+df <- read_sheet("https://docs.google.com/spreadsheets/d/1qiFlx8GLiMoJjtfWzlzVu7km1XCnWXjWCVAhmCtI-j0/edit#gid=0")%>%
+  mutate(Date = as.character(Date))
+
+write.csv(df, here("Leaf_Map/Leaf_Data.csv")) # Save updated table locally
+
+# Read in local table and exclude rows with no coordinates
+locs <- read.csv(here("Leaf_Map/Leaf_Data.csv"))%>%
+  select(Date,Lon,Lat)%>%
+  drop_na()%>%
+  left_join(df)
+
+
+# Create spatial dataset
+
+sf <- st_as_sf(locs, coords = c("Lon", "Lat"), 
+               crs = 4326, agr = "constant")
+
+# Create Leaf Icon
+greenLeafIcon <- makeIcon(
+  iconUrl = "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+  iconWidth = 38, iconHeight = 95,
+  iconAnchorX = 22, iconAnchorY = 94,
+  shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+  shadowWidth = 50, shadowHeight = 64,
+  shadowAnchorX = 4, shadowAnchorY = 62
+)
+
+map <- leaflet(data = sf) %>% addTiles() %>%
+  addMarkers(icon = greenLeafIcon, popup = ~paste(
+    First,Last, "<br>",
+    Institution, "<br>",
+    City," (",Country,")","<br>",
+    "<b><a href=",Leaf_URL,">Leaf Page</a></b>","<br>",
+    "<b><a href=",Website,">Website</a></b>"
+    ))
+
+#search <- addSearchFeatures(map,~c(sf$Institution,sf$Last), options = searchFeaturesOptions())
+
+saveWidget(map, file=here("Leaf_Map/Leaf_Map.html"))
+
+#carto-positron
